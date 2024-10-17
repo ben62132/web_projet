5bc080f4a65c4581880600fb559536de3c41dbae
Benjamin <benjamin.marcadet@groupe-esigelec.org>
2024-10-17 11:14:42 +0200

    Création de la page d'accueil

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html> 
+
+<html> 
+
+    <head> 
+
+        <meta charset="utf-8"> 
+
+        <title>Titre affiché dans l’onglet</title> 
+
+    </head> 
+
+    <body> 
+
+        Contenu de la page zgoubinistre 
+
+    </body> 
+
+</html> 
+
+ 
