2aec93219cfa1c2e99e70e45b5af4f9ac4d1ba2f
Dubus <zacharie.dubus@groupe-esigelec.org>
2024-11-04 13:59:27 +0100

    new de fou

--- header.inc.php
+++ header.inc.php
@@ -1,43 +1,9 @@
-<!DOCTYPE html>
-<html lang="fr">
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php echo $titre; ?></title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<link rel="stylesheet" href="style.css">
-<div id="menu">
-<a href="page.php">
-<img src="./imports/logopng.png" class="logo rounded float-left" alt="...">
-</a>
-
-</div>
-<nav class="navbar navbar-expand-md border-bottom border-body" >
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Esigelec</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="index.php">Accueil</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="page.php">Page</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="autre.php">Autre page</a>
-        </li>
-      </ul>
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="inscription.php">Inscription</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" hre ="connexion.php">Connexion</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-<div class="container">
+<meta charset="utf-8">
+    <meta name="Maxi test" content="width=device-width, initial-scale=1">
+    <title>Maxi Test</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
--- index.php
+++ index.php
@@ -1,28 +1,59 @@
-<!doctype html> 
+<!doctype html>
+<html lang="fr">
 
-<html> 
-
-    <head> 
+<head>
     
-        <?php
-            include ('headernew.inc.php');
+</head>
+<?php  
+            include ('header.inc.php');
             ?>
-
-        <meta charset="utf-8"> 
-
-        <title>Titre affiché dans l’onglet</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css" type="text/css">
-
-    </head> 
+<body>
+    <header class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col">
+                <img src="logopng.png" alt="Logo" class="logo">
+            </div>
+            <div class="col text-end">
+                <a href="" class="connexion">Se connecter</a>
+            </div>
+        </div>
+    </header>
     
-    <body style ="background-color:black"> 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <div class="texte"> <!-- Ajout de la classe "texte" -->
-        Salut les zoulettes de la mort
+    <div class="container page-main">
+        <div class="text-center titre">
+            <h2>Entrainements</h2>
+        </div>
+        
+        <div class="row align-items-center mb-4">
+            <div class="col-md-6">
+                <img src="entrainement.jpg" class="img-fluid" alt="Entrainement">
+            </div>
+            <div class="col-md-6">
+                <h1>Titre</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis molestiae doloribus iste eligendi
+                    neque vitae! Sint nihil sequi saepe magni neque id tempore sed? Qui sed esse cumque cupiditate
+                    perspiciatis.
+                </p>
+            </div>
+        </div>
+        
+        <div class="row align-items-center mb-4">
+            <div class="col-md-6 order-md-2">
+                <img src="entrainement2.jpg" class="img-fluid" alt="Entrainement 2">
+            </div>
+            <div class="col-md-6">
+                <h1>Titre</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis molestiae doloribus iste eligendi
+                    neque vitae! Sint nihil sequi saepe magni neque id tempore sed? Qui sed esse cumque cupiditate
+                    perspiciatis.
+                </p>
+            </div>
+        </div>
     </div>
-    </body> 
 
-</html> 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
+</body>
 
- 
+</html>
