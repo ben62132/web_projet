116f79751f4e0a0182b14de9d5706ff5112dc463
Dubus <zacharie.dubus@groupe-esigelec.org>
2024-11-04 13:23:39 +0100

    new

--- header.inc.php
+++ header.inc.php
@@ -6,13 +6,12 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
 <div id="menu">
-<li><a href="page.php">Accueil</a></li>
 <a href="page.php">
-<img src="./imports/logo.png" class="logo rounded float-left" alt="...">
+<img src="./imports/logopng.png" class="logo rounded float-left" alt="...">
 </a>
 
 </div>
-<nav class="navbar navbar-expand-md bg-dark border-bottom border-body" >
+<nav class="navbar navbar-expand-md border-bottom border-body" >
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php">Esigelec</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
