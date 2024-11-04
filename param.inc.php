3e1901e71ff3331e6645168ab90d26b2f9633dda
Dubus <zacharie.dubus@groupe-esigelec.org>
2024-11-04 14:23:23 +0100

    maj bdd

--- param.inc.php
+++ param.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-  $host="localhost";
-  $login="root";
-  $passwd="root";
-  $dbname="projet_web";
+  $host="localhost3306";
+  $login="grp_3_4";
+  $passwd="2PCLR7aKE3RY1a";
+  $dbname="bdd_3_4";
 
   
 ?>
