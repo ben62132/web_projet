efd30d93fff2ae177c197344cf7701bf6a2962df
Benjamin <benjamin.marcadet@groupe-esigelec.org>
2024-11-04 17:11:45 +0100

    qfv

---
+++ a_propos_de_nous.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <?php  
+            include ('head.inc.php');
+            ?>
+</head>
+
+<body>
+    <header class="container-fluid">
+        
+        <?php
+            include ('header.inc.php');
+        ?>
+    </header>
+    
+    <footer class="container-fluid">
+    <?php
+        include('footer.inc.php');
+        ?>
+    </footer>
+</body>
+
+</html>
---
+++ footer.inc.php
@@ -0,0 +1,29 @@
+<div class="row align-items-center blabla">
+            <div class="col">
+                <img 
+                    src="adidas.png" alt="Logo adidas" class="logo_adidas img-fluid"/>
+            </div>
+            <div class="col">
+                <img 
+                    src="asics.png" alt="Logo asics" class="logo_asics img-fluid" />
+            </div>
+            <div class="col">
+                <img 
+                    src="lacoste.png" alt="Logo lacoste" class="logo_lacoste img-fluid" />
+            </div>
+            <div class="col">
+                <img 
+                    src="nike.png" alt="Logo nike" class="logo_nike img-fluid" />
+            </div>
+            <div class="col">
+                <img 
+                    src="puma.png" alt="Logo puma" class="logo_puma img-fluid" />
+            </div>
+            <div class="col">
+                <img 
+                    src="reebok.png" alt="Logo reebok" class="logo_reebok img-fluid" />
+            </div>
+            <div class="">
+                <a href="a_propos_de_nous.php" class="a_propos_de_nous">A propos de nous</a>
+            </div>
+        </div>
