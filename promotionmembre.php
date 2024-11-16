24ec48439601942d8d9a11a8215652ffab72e0ff
Benjamin <benjamin.marcadet@groupe-esigelec.org>
2024-11-16 19:39:21 +0100

    h

--- promotionmembre.php
+++ promotionmembre.php
@@ -22,6 +22,7 @@ include('promotionmembre.inc.php');
 
 <div>
   <h1>Promouvoir un membre</h1>
+  <?php foreach ($utilisateurs as $utilisateur): ?>
         <div class="row align-items-center mb-4">
                 <div class="col-md-6">
                     <h1><?= htmlspecialchars($utilisateurs['utilisateur_nomUtilisateur']) ?></h1>
